cb1560a8152e93c623e0462f7b30144f5b6aeb83
EKinsella <kinsella.eoin@gmail.com>
2016-10-21 10:51:50 +0100

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <h1>Toucan Tech</h1>
+        <p class="lead">Toucan Tech assessment using ASP.NET, C# and SQL.</p>
+    </div>
+
+    <div class="row">
+        <div class="col-md-4">
+            <h2>New Member</h2>
+            <p>
+                <asp:Label ID="NameLbl" runat="server" Text="Name">Name:   &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp</asp:Label><asp:TextBox ID="NameTextBox" runat="server"></asp:TextBox>   
+            </p>
+            <p>
+                <asp:Label ID="EmailAddrLbl" runat="server" Text="Email">Email Address: &nbsp </asp:Label><asp:TextBox ID="EmailTextBox" runat="server"></asp:TextBox>
+            </p>
+            <p>
+                <asp:Label ID="SchoolLbl" runat="server" Text="School">School: &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp </asp:Label><asp:DropDownList ID="DropDownList1" runat="server" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+                                                                                                                                        <asp:ListItem>St. Trinians</asp:ListItem>
+                                                                                                                                        <asp:ListItem>Kettering Community College</asp:ListItem>
+                                                                                                                                        <asp:ListItem>St George's</asp:ListItem>
+                                                                                                                                        <asp:ListItem>St. Matthews</asp:ListItem>
+                                                                                                                                        <asp:ListItem>Coventry Community College</asp:ListItem>
+                                                                                                                                        <asp:ListItem>St Mary's</asp:ListItem>
+                                                                                                                                     </asp:DropDownList>
+            </p>
+            <p>
+                <asp:Button ID="RegisterButton" runat="server" Text="Register" OnClick="RegisterButton_Click" />
+            </p>
+        </div>
+    </div>
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+public partial class _Default : Page
+{
+    SqlConnection connection = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\Eoin\Documents\Visual Studio 2013\WebSites\WebSite2\App_Data\Database.mdf;Integrated Security=True");
+    /*protected void Page_Load(object sender, EventArgs e)
+    {
+        
+    }*/
+    protected void RegisterButton_Click(object sender, EventArgs e)
+    {
+        SqlCommand command = new SqlCommand();
+        String query = "INSERT INTO member VALUES(@Name, @Email, @School)";
+        
+        int MemberId = 0;
+
+        using (SqlCommand cmd = new SqlCommand("AddMember"))
+        {
+            using (SqlDataAdapter adapter = new SqlDataAdapter())
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Id", MemberId);
+                cmd.Parameters.AddWithValue("@Name", NameTextBox.Text.Trim());
+                cmd.Parameters.AddWithValue("@Email", EmailTextBox.Text.Trim());
+                cmd.Parameters.AddWithValue("@School", SchoolLbl.Text.Trim());
+                cmd.Connection = connection;
+                connection.Open();
+                MemberId = MemberId ++;
+                //cmd = new SqlCommand(query, connection);
+                //cmd.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+
+        String messge = String.Empty;
+        switch (MemberId)
+        { 
+            case 1:
+                messge = "Name already exists.\nPlease try a different name.";
+                break;
+            case 2:
+                messge = "Email address is already used.\nPlease try a different email address.";
+                break;
+            default:
+                messge = "Registration complete.";
+                break;
+        }
+        ClientScript.RegisterStartupScript(GetType(), "alert", "alert('" + messge + "');", true);
+    }
+    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        connection.ConnectionString = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\Eoin\Documents\Visual Studio 2013\WebSites\WebSite2\App_Data\Database.mdf;Integrated Security=True";
+
+        using (SqlCommand cmd = new SqlCommand("Procedure2"))
+        {
+            using (SqlDataAdapter adapter = new SqlDataAdapter())
+            {
+                connection.Open();
+                String addstr = "SELECT Id, name, emailaddress, school FROM member";
+                SqlCommand command = new SqlCommand(addstr, connection);
+                DataTable table = new DataTable();
+                command.CommandType = CommandType.Text;
+                DropDownList1.DataSource = table;
+                DropDownList1.DataTextField = "School";
+                DropDownList1.DataValueField = "School";
+                DropDownList1.DataBind();
+
+                for (int i = 0; i < DropDownList1.Items.Count; i++)
+                {
+                    if (DropDownList1.Items[i].Selected == true)
+                    {
+                        String str = "INSERT INTO member VALUES(@School)";
+                        command = new SqlCommand(str, connection);
+                        command.ExecuteNonQuery();
+
+                    }
+                }
+            }
+        }
+        connection.Close();
+    }
+}
